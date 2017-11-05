9c4f6e4b6d9ea43aef3866e67e48204657b7dc8c
Alexander Chebykin <a.e.chebykin@gmail.com>
2017-11-05 18:24:07 +0300

    Delete consecutive repetitions; minor optimization: less zipping and unzipping

--- my_utils.py
+++ my_utils.py
@@ -16,7 +16,7 @@ def create_dict_on_condition(sentence_list, predicate):
     return word_set
 
 
-def clean_up_sentences(list_of_sentence_pairs, api_dict=None):
+def clean_up_sentences(list_of_sentence_pairs, api_dict=None, if_shorten=False, if_remove_repetitions=False):
     def clean_up_eng(sentence_eng):
         sentence_eng[:] = [word.lower().translate(str.maketrans('', '', string.punctuation)) for word in sentence_eng if
                            len(word) < 20]
@@ -24,8 +24,10 @@ def clean_up_sentences(list_of_sentence_pairs, api_dict=None):
         # for i, word in enumerate(words):
         #     words[i] = word.lower().translate(None, string.punctuation)
 
-    def clean_up_api(sentence_api):
+    def clean_api_by_dict(sentence_api):
         sentence_api[:] = [call for call in sentence_api if call in api_dict]
+
+    def clean_api_shorten(sentence_api):
         sentence_word_boundary = 10
         if len(sentence_api) >= sentence_word_boundary:
             words_at_ends_to_keep = 3
@@ -33,17 +35,32 @@ def clean_up_sentences(list_of_sentence_pairs, api_dict=None):
             end = sentence_api[len(sentence_api) - words_at_ends_to_keep:]
             sentence_api[:] = start + end
 
+    def clean_api_consecutive_repetitions(sentence_api):
+        prev = None
+        res = []
+        for word in sentence_api:
+            if word != prev:
+                res.append(word)
+            prev = word
+        sentence_api[:] = res
+
     for sentence_pair in list_of_sentence_pairs:
         clean_up_eng(sentence_pair[0])
         if api_dict is not None:
-            clean_up_api(sentence_pair[1])
+            clean_api_by_dict(sentence_pair[1])
+        if if_shorten:
+            clean_api_shorten(sentence_pair[1])
+        if if_remove_repetitions:
+            clean_api_consecutive_repetitions(sentence_pair[1])
     list_of_sentence_pairs[:] = [(eng, api) for (eng, api) in list_of_sentence_pairs if len(eng) > 0 and len(api) > 0]
-    return zip(*list_of_sentence_pairs)
+    # return zip(*list_of_sentence_pairs)
+    return list_of_sentence_pairs
 
 
 def separate_to_train_and_dev(eng_api_list):
     total = len(eng_api_list)
-    prob_boundary = 20000.0 / float(total)
+    test_size = 20000.0
+    prob_boundary = test_size / float(total)
     dev = []
     train = []
     for pair in eng_api_list:
@@ -57,13 +74,12 @@ def separate_to_train_and_dev(eng_api_list):
 def parse_file_to_eng_and_api(filename="data.txt",
                               ifcleanup=True,
                               leave_only_system=False):
-    english_desc = []
-    api_desc = []
+    eng_api = []
     # unwanted_comments = [
     #     'Required method for Designer support - do not modify  the contents of this method with the code editor.',
     #     'Clean up any resources being used.',
     #     'The main entry point for the application.']
-    designer_comment_start = 'Required method for Designer support'
+    designer_comment_start = 'Required method for Designer'
     with open(filename, "r", encoding='utf-8') as f:
         while True:
             line = f.readline().strip()
@@ -82,22 +98,23 @@ def parse_file_to_eng_and_api(filename="data.txt",
             cur_api = line.split(" ")
 
             if not (len(cur_api) == 0 or len(cur_eng) == 0):
-                api_desc += [cur_api]
-                english_desc += [cur_eng]
+                eng_api.append((cur_eng, cur_api))
 
     if ifcleanup:
         api_dict = None
         if leave_only_system:
+            _, api_desc = zip(*eng_api)
             api_dict = create_dict_on_condition(api_desc, lambda x: x.startswith("System.") or x.count('.') == 1)
-        english_desc, api_desc = clean_up_sentences(list(zip(english_desc, api_desc)), api_dict)
+        eng_api = clean_up_sentences(eng_api, api_dict, if_remove_repetitions=True)
 
     # make every pair unique
-    temp = set(zip(map((lambda x: tuple(x)), english_desc), map((lambda x: tuple(x)), api_desc)))
-    english_desc, api_desc = zip(*list(temp))
+    # temp = set(zip(map((lambda x: tuple(x)), english_desc), map((lambda x: tuple(x)), api_desc)))
+    return list(set(map((lambda x: (tuple(x[0]), tuple(x[1]))), eng_api)))
+    # english_desc, api_desc = zip(*list(temp))
 
     # no writing to files!
 
-    return english_desc, api_desc
+    # return english_desc, api_desc
 
 
 def write_all_train_data_to_files(english_desc, api_desc, engfile="eng.txt", apifile="api.txt", ifoverwrite=False):
@@ -116,14 +133,13 @@ def parse_res_files_to_train_and_test(res_files=None, res_files_count=2, res_fil
         res_files = ['/tmp/res' + str(i) + '.txt' for i in
                      range(res_files_first, res_files_count + 1)]  # ['/tmp/res1.txt', '/tmp/res2.txt']
 
-    eng, api = parse_file_to_eng_and_api(filename=res_files[0])
+    eng_api = parse_file_to_eng_and_api(filename=res_files[0])
     for res_file in res_files[1:]:
         print('parsing ' + res_file)
-        eng_new, api_new = parse_file_to_eng_and_api(filename=res_file)
-        eng += eng_new
-        api += api_new
+        eng_api_new = parse_file_to_eng_and_api(filename=res_file)
+        eng_api += eng_api_new
 
-    train, dev = separate_to_train_and_dev(list(zip(eng, api)))
+    train, dev = separate_to_train_and_dev(eng_api)
     # remove_empty_lines.remove_pair_where_one_is_empty(train)
     # remove_empty_lines.remove_pair_where_one_is_empty(dev)
     eng_train, api_train = zip(*train)
@@ -176,11 +192,16 @@ def write_two_vocabs(eng_vocab, api_vocab, eng_size=10000, api_size=10000, vocab
     utils.write_lines_to_file('vocab' + str(api_size) + '_test' + vocab_postfix + '.to', api_vocab)
 
 if __name__ == "__main__":
-    # a = [('ab', 'cd'), ('ef')]
-    train_eng_file = 'train8.eng'
-    train_api_file = 'train8.api'
-    test_eng_file = 'test8.eng'
-    test_api_file = 'test8.api'
+    #a = [('ab', 'cd'), ('ef')]
+    # b = [('b', 'd'), ('f')]
+    # c = zip(*zip(a, b))
+    # a = [1, 3, 5, 5, 6, 7, 8]
+    # b = a[1:]
+    # c = a[:-1]
+    train_eng_file = 'train9.eng'
+    train_api_file = 'train9.api'
+    test_eng_file = 'test9.eng'
+    test_api_file = 'test9.api'
     (eng_train_samples, eng_test_samples), (api_train_samples, api_test_samples) = \
         parse_res_files_to_train_and_test(res_files_count=24)
 
@@ -189,7 +210,7 @@ if __name__ == "__main__":
     api_vocab_size = 1000
     api_vocab = create_vocab(api_train_samples, api_vocab_size, 0)
 
-    write_two_vocabs(eng_vocab, api_vocab, vocab_postfix='max+dict1000')
+    write_two_vocabs(eng_vocab, api_vocab, vocab_postfix='max+dict1000+noAPIrepeat')
 
     # eng_train_samples = filter_list_of_lists_by_vocab(eng_train_samples, eng_vocab)
     # eng_test_samples = filter_list_of_lists_by_vocab(eng_test_samples, eng_vocab)
